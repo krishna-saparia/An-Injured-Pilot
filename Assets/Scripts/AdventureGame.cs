8fc1d6ea6fd71a42684cce5ddcca42ad0aae00d1
krishna saparia <krishnasaparia43045@gmail.com>
2021-01-18 20:50:08 -0500

    A finalized WebGL Game of Injured Pilot is deploying.

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Text textComponent;
+    [SerializeField] State StartingState;
+    State state;
+    void Start()
+    {
+        state = StartingState;    
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+              
+            }
+        }
+       textComponent.text = state.GetStateStory();
+    }
+
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(15, 11)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
